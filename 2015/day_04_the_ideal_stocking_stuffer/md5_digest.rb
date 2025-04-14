12392d08968ef69795f95de92e92d2839694ebb1
Juliette Audema <juliette.audema@gmail.com>
2025-04-14 15:47:17 +0200

    Day 4: The Ideal Stocking Stuffer - Part 2

--- 2015/day_04_the_ideal_stocking_stuffer/md5_digest.rb
+++ 2015/day_04_the_ideal_stocking_stuffer/md5_digest.rb
@@ -1,12 +1,13 @@
 require 'digest'
 
 class MD5Digest
-  def self.perform(secret_key)
+  def self.perform(secret_key, zero_count)
     number = 1
+    zeros = '0' * zero_count
 
     loop do
       hash = Digest::MD5.hexdigest("#{secret_key}#{number}")
-      return number if hash.start_with?("00000")
+      return number if hash.start_with?(zeros)
       number += 1
     end
   end
--- 2015/day_04_the_ideal_stocking_stuffer/md5_digest_test.rb
+++ 2015/day_04_the_ideal_stocking_stuffer/md5_digest_test.rb
@@ -2,18 +2,27 @@ require 'minitest/autorun'
 require_relative 'md5_digest'
 
 class MD5DigestTest < Minitest::Test
-  def test_secret_key_abcdef
+  def test_secret_key_abcdef_5_zeros
     secret_key = 'abcdef'
-    assert_equal 609043, MD5Digest.perform(secret_key)
+    zero_count = 5
+    assert_equal 609043, MD5Digest.perform(secret_key, zero_count)
   end
 
-  def test_secret_key_pqrstuv
+  def test_secret_key_pqrstuv_5_zeros
     secret_key = 'pqrstuv'
-    assert_equal 1048970, MD5Digest.perform(secret_key)
+    zero_count = 5
+    assert_equal 1048970, MD5Digest.perform(secret_key, zero_count)
   end
 
-  def test_secret_key_yzbqklnj
+  def test_secret_key_yzbqklnj_5_zeros
     secret_key = 'yzbqklnj'
-    assert_equal 282749, MD5Digest.perform(secret_key)
+    zero_count = 5
+    assert_equal 282749, MD5Digest.perform(secret_key, zero_count)
+  end
+
+  def test_secret_key_yzbqklnj_6_zeros
+    secret_key = 'yzbqklnj'
+    zero_count = 6
+    assert_equal 9962624, MD5Digest.perform(secret_key, zero_count)
   end
 end
