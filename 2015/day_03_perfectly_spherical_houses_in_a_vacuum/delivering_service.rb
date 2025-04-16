222a111d32a7914797ace5a3c0fa51dbafb999f3
Juliette Audema <juliette.audema@gmail.com>
2025-04-16 14:32:46 +0200

    Refine Day 03

--- 2015/day_03_perfectly_spherical_houses_in_a_vacuum/delivering_service.rb
+++ 2015/day_03_perfectly_spherical_houses_in_a_vacuum/delivering_service.rb
@@ -1,44 +1,36 @@
 require 'pry'
 
 class DeliveringService
-  def initialize(directions)
+  def initialize(directions, with_robo:false)
     @directions = directions
-    @position = [0,0]
-    @map = {}
-    @map[@position] = 1
-    @x, @y = @position
+    @with_robo = with_robo
+    @positions = [[0, 0], [0, 0]]
+    @map = Hash.new(0)
+    @map[[0, 0]] = 1
+
   end
 
   def houses_visited
-    @directions.each do |direction|
-      case direction
-      when '>'
-        @x += 1
-        @position = [@x, @y]
-        @map[@position] ||= 0
-        @map[@position] += 1
-      when '<'
-        @x -= 1
-        @position = [@x, @y]
-        @map[@position] ||= 0
-        @map[@position] += 1
-      when '^'
-        @y += 1
-        @position = [@x, @y] 
-        @map[@position] ||= 0
-        @map[@position] += 1
-      when 'v'
-        @y -= 1
-        @position = [@x, @y] 
-        @map[@position] ||= 0
-        @map[@position] += 1
-      end
+    @directions.chars.each_with_index do |direction, index|
+      mover_index = @with_robo ? index % 2 : 0
+      move(@positions[mover_index], direction)
+      @map[@positions[mover_index].dup] += 1
     end
-
     @map
   end
 
   def number_of_houses_visited
-    houses_visited.keys.count
+    houses_visited.keys.size
+  end
+
+  private
+
+  def move(position, direction)
+    case direction
+    when '>' then position[0] += 1
+    when '<' then position[0] -= 1
+    when '^' then position[1] += 1
+    when 'v' then position[1] -= 1
+    end
   end
 end
