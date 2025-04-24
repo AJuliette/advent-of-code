51e069e1e009bab86680ac155a263f9d30b0a19d
Juliette Audema <juliette.audema@gmail.com>
2025-04-24 13:11:17 +0200

    Resolve Day 7: Some Assembly Required - Part 2

--- 2015/day_07_some_assembly_required/main.rb
+++ 2015/day_07_some_assembly_required/main.rb
@@ -4,7 +4,16 @@ class Main
   def self.perform
     input = File.read('input.txt').lines.map(&:strip)
     circuit = Circuit.new(input)
-    puts "Signal to wire a: #{circuit.signal('a')}"
+    a_value = circuit.signal('a')
+    puts "Part 1: wire a = #{a_value}"
+
+    modified_input = input.map do |line|
+      line.end_with?('-> b') ? "#{a_value} -> b" : line
+    end
+
+    circuit_2 = Circuit.new(modified_input)
+    new_a_value = circuit_2.signal('a')
+    puts "Part 2: new wire a = #{new_a_value}"
   end
 end
 
